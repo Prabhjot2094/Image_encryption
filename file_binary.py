99b68df7e0aedf207c8dad231e6b0d377e071c48
Prabhjot2094 <prabhjotsingh467@gmail.com>
2016-09-17 21:25:56 +0530

    All encryptions complete without custom password

--- file_binary.py
+++ file_binary.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from Crypto.Cipher import AES
+from Crypto.Cipher import DES
+import pyDes
 import rsa
+import time
 import sys
 import binascii
 import math
@@ -12,7 +15,10 @@ def format_data(hex_str):
     hex_str = binascii.unhexlify(hex_str)
     return hex_str
 
-def calc_dim(file_size):
+def calc_dim(file_size , enc_type):
+
+    if enc_type=='rsa':
+        file_size = file_size*32/21
     added_bytes = file_size%3
     t1 = file_size/3
     t2 = pow(t1,0.5)
@@ -27,7 +33,7 @@ def calc_dim(file_size):
     return x,y
 
 def calc_dim_rsa(file_size):
-    file_size = file_size*2
+    file_size = file_size*no_of_rgbs/bytes_to_read
     added_bytes = file_size%3
     t1 = file_size/3
     t2 = pow(t1,0.5)
@@ -40,12 +46,51 @@ def calc_dim_rsa(file_size):
     else:
         x=y=t2
     return x,y
-(public_key, private_key) = rsa.newkeys(256)
 
-def enc():
-    file_name = 'test4.jpg' 
+def get_img_ext(height , im):
+    ext_len,e1,e2 = im.getpixel((2,height-1))
+    ext_str = hex(e1)[2:]+hex(e2)[2:]
+    i=0
+    j=3
+    while 1:
+        rgb = im.getpixel((j,height-1))
+        i+=3
+        if i>ext_len-2:
+            diff = i-ext_len+2
+            print "diff = " , diff
+            for val in range(diff+1):
+                print "val = ",val
+                h = hex(rgb[val])[2:]
+                if len(h)==1:
+                    h ='0'+h
+                ext_str+=h 
+            break
+        else:
+            for t in range(3):
+                h = hex(rgb[t])[2:]
+                if len(h)==1:
+                    h ='0'+h
+                ext_str+=h
+        j+=1
+    ext = binascii.unhexlify(ext_str)
+    return ext
+
+(public_key, private_key) = rsa.newkeys(256)
+def enc(enc_type , file_name):
+    print public_key
+    print private_key
+    if enc_type=='rsa':
+        bytes_to_read = 21
+        no_of_rgbs = 32
+    elif enc_type=='aes' or enc_type=='des':
+        if enc_type=='des':
+            obj = DES.new("DESCRYPT", DES.MODE_CBC,'\0\0\0\0\0\0\0\0')
+        bytes_to_read = 16
+        no_of_rgbs = 16
+
+    f_name , ext = os.path.splitext(file_name)
     file_size = os.path.getsize(file_name)
-    width,height = calc_dim_rsa(file_size)
+    width,height = calc_dim(file_size , enc_type)
 
     print width,height
     height+=1
@@ -56,7 +101,6 @@ def enc():
     final_bytes = False
     i=j=prev_col=x=0
     p_arr=[0,0,0]
-    enc_type = 'aes'
     padding = '123456789101312'
 
     s=0
@@ -64,84 +108,129 @@ def enc():
         s+=1
        # print s
         ''' i+=1
-        if i>213:
-            i=i%213
+        if i>bytes_to_read3:
+            i=i%bytes_to_read3
             j+=1
         '''
         #print j,i
 
-        z = f.read(16)
-        if len(z)!=16:
+        z = f.read(bytes_to_read)
+        if len(z)!=bytes_to_read:
             #print len(z)
-            t1 = 16-len(z)
+            t1 = bytes_to_read-len(z)
             extra_bytes = len(z)
             z+=padding[:t1]
             final_bytes = True
-        if enc_type=='rsa':
-            g.write(rsa.encrypt(z,priv))
-        else:
-            if s==1 or s==2 or s==3:
-                print "Hex just before encryption = ",binascii.hexlify(z)
+        
+        if s==1 or s==2 or s==3:
+            print "Hex just before encryption = ",binascii.hexlify(z)
+
+        if enc_type=='aes':
             obj = AES.new('aaaaaaaaaaaaaaaa', AES.MODE_CBC, 'This is an IV456')
+            encrypted_hex = obj.encrypt(z)
+        elif enc_type=='rsa':
             encrypted_hex = rsa.encrypt(z,public_key)
-            encrypted_hex = binascii.hexlify(encrypted_hex)
-            if s==1 or s==2 or s==3 or final_bytes==True:
-                print "Hex after encryption = ",encrypted_hex
-                print "Hex length after encryption = ",len(encrypted_hex)
+        elif enc_type=='des':
+            encrypted_hex = obj.encrypt(z)
+        
+        encrypted_hex = binascii.hexlify(encrypted_hex)
+        
+        if s==1 or s==2 or s==3 or final_bytes==True:
+            print "Hex after encryption = ",encrypted_hex
+            print "Hex length after encryption = ",len(encrypted_hex)
 
-            rgb_arr = []
+        rgb_arr = []
 
-            t=0
-    
-            for d in range(32):
-                rgb_arr.append(int(encrypted_hex[t:t+2] , 16))
-                t+=2
-            for pixel in range(32):
-                        p_arr[x] = rgb_arr[pixel]
-                        x+=1
-                        if final_bytes==True:
-                            if pixel==t1:
-                                rgb_pos = x-1
-                                last_pixel_col = prev_col
-                                last_pixel_row = j
-                                print last_pixel_row
-                        if x>2:
-                            try:
-                                im.putpixel((prev_col,j),(p_arr[0],p_arr[1],p_arr[2]))
-                            except:
-                                print prev_col,j
-                                sys.exit()
-                            x%=3
-                            prev_col = (prev_col+1)
-                            i=prev_col
-                            if prev_col>width-1:
-                                j+=1
-                            prev_col%=width
-                            p_arr = [0,0,0]
+        t=0
+
+        for d in range(no_of_rgbs):
+            rgb_arr.append(int(encrypted_hex[t:t+2] , 16))
+            t+=2
+        for pixel in range(no_of_rgbs):
+                    p_arr[x] = rgb_arr[pixel]
+                    x+=1
+                    if final_bytes==True:
+                        if pixel==t1:
+                            rgb_pos = x-1
+                            last_pixel_col = prev_col
+                            last_pixel_row = j
+                            print last_pixel_row
+                    if x>2:
+                        im.putpixel((prev_col,j),(p_arr[0],p_arr[1],p_arr[2]))
+                        x%=3
+                        prev_col = (prev_col+1)
+                        i=prev_col
+                        if prev_col>width-1:
+                            j+=1
+                        prev_col%=width
+                        p_arr = [0,0,0]
         if final_bytes==True:
             print p_arr
             if p_arr!=[0,0,0]:
                 im.putpixel((prev_col,j),(p_arr[0],p_arr[1],p_arr[2]))
             break
+    
     t2 = prev_col/256
     t3 = prev_col%256
     im.putpixel((0,height-1),(t2,t3,extra_bytes))
+    
     t1 = last_pixel_row/256
     t2 = last_pixel_row%256
     im.putpixel((1,height-1),(t1,t2,0))
     
-    for i in range(2,width):
+    ext_len = len(ext)
+    ext = binascii.hexlify(ext)
+    print "Ext = ",ext
+
+    i=x=0
+    count=1
+    prev_col = 2
+    p_arr[0] = ext_len
+    x+=1
+    while count<=ext_len:
+            print i,count , x
+            count+=1
+            p_arr[x] = int(ext[i:i+2],16)
+            i+=2
+            x+=1
+            if x>2:
+                print p_arr,i
+                im.putpixel((prev_col,height-1),(p_arr[0],p_arr[1],p_arr[2]))
+                x%=3
+                prev_col = (prev_col+1)
+                prev_col%=width
+                p_arr = [0,0,0]
+   
+    if p_arr!=[0,0,0]:
+            print p_arr
+            im.putpixel((prev_col,height-1),(p_arr[0],p_arr[1],p_arr[2]))
+            prev_col+=1
+    
+    for i in range(prev_col,width):
             im.putpixel((i,height-1),(255,1,231))
     im.save("file_image.png")
 
-def dec():
-    im = Image.open("file_image.png")
-    f = open("decrypted_image.jpg",'wb')
+def dec(enc_type , enc_file_name , dec_file_name):
+   # private_key = rsa.PrivateKey(95150004249211316623377898446877566485091194414253641389766719556945143338421, 65537, 39732825217635322062834474370137454566587906700281906704417610768193167777173, 75528358950466226404643885286786069526691, 1259791759961494151337324382202521031)
+    im = Image.open(enc_file_name)
     width,height = im.size
+    ext = get_img_ext(height , im)
+
+    dec_file_name += ext
+    f = open(dec_file_name,'wb')
     
+    if enc_type=='rsa':
+        bytes_to_read = 21
+        no_of_rgbs = 32
+    elif enc_type=='aes' or enc_type=='des':
+        if enc_type == 'des':
+            obj = DES.new("DESCRYPT", DES.MODE_CBC,'\0\0\0\0\0\0\0\0')
+        bytes_to_read = 16
+        no_of_rgbs = 16
 
     t1,t2,t3 = im.getpixel((0,height-1))
     k1,k2 = im.getpixel((1,height-1))[:2]
+
 
     t4 = (t1*256)+t2
     k3 = (k1*256)+k2
@@ -163,24 +252,30 @@ def dec():
         rgb_arr.append(b)
         x+=3
 
-        if x>32:
-            for z in range(32):
+        if x>no_of_rgbs:
+            for z in range(no_of_rgbs):
                 hex_val = hex(rgb_arr.pop(0))
                 if len(str(hex_val))==3:
                     if i==t4 and j==k3:
                         print hex_val
                     hex_val = hex_val[:2]+'0'+hex_val[2:]
                 hex_str += hex_val 
-            x%=32
+            
+            x%=no_of_rgbs
             hex_str = format_data(hex_str)
-            #hex_str = binascii.hexlify(hex_str)
-            #rint binascii.hexlify(hex_str)
-            #hex_str = binascii.unhexlify(hex_str)
+            
             if d==0 or d==1 or d==2 or (i==t4 and j==k3):
                 print "hex before decryption = ", binascii.hexlify(hex_str)
                 print "hex length before decryption = ", len(binascii.hexlify(hex_str))
-            obj = AES.new('aaaaaaaaaaaaaaaa', AES.MODE_CBC, 'This is an IV456')
-            decrypted_hex = rsa.decrypt(hex_str,private_key)
+            
+            if enc_type=='aes':
+                obj = AES.new('aaaaaaaaaaaaaaaa', AES.MODE_CBC, 'This is an IV456')
+                decrypted_hex = obj.decrypt(hex_str)
+            elif enc_type=='rsa':
+                decrypted_hex = rsa.decrypt(hex_str,private_key)
+            elif enc_type=='des':
+                decrypted_hex = obj.decrypt(hex_str)
+            
             if i==t4 and j==k3:
                 f.write(decrypted_hex[:t3])
                 print "Breaking at ", i, j
@@ -188,7 +283,7 @@ def dec():
             if d==0 or d==1 or d==2:
                 d+=1
                 print "hex rigth after decryption = ", binascii.hexlify(decrypted_hex)
-            f.write(decrypted_hex[:16])
+            f.write(decrypted_hex[:bytes_to_read])
             hex_str = ''
         i+=1
         if i>=width:
@@ -196,119 +291,13 @@ def dec():
             j+=1
     f.close()
 
+t1 = time.time()
+#enc('rsa' , "test2.jpg")
 
-
-
-
-
-
-
-
-
-
-enc()
-dec()
+#enc('des' , "file_image.png")
+t2 = time.time()
+#print t2-t1
+dec('rsa' ,"file_image.png" , "decrypted_image")
+#dec('aes' ,"file_image1.png" , "decrypted_image")
+print time.time()-t2
 sys.exit()
-'''
-f = open('glassball.png' , 'rb')
-g = open('etest2.png' , 'wb')
-h = open('dtest2.png' , 'wb')
-
-im1 = Image.new("RGB" , (213, 213))
-im2 = Image.new("RGB" , (213, 213))
-
-(pub,priv) = rsa.newkeys(256)
-
-obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-
-i=j=prev_col=x=0
-p_arr=[0,0,0]
-enc_type = 'aes'
-
-s=0
-while 1:
-        s+=1
-       # print s
-         i+=1
-        if i>213:
-            i=i%213
-            j+=1
-        
-        #print j,i
-
-        obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-        z = f.read(16)
-        if len(z)!=16:
-            pass
-        if enc_type=='rsa':
-            g.write(rsa.encrypt(z,priv))
-        else:
-            encrypted_hex = obj.encrypt(z)
-            if s==1:
-                print "Hex after encryption = ",encrypted_hex
-            encrypted_hex = binascii.hexlify(encrypted_hex)
-            rgb_arr = []
-
-            t=0
-    
-            for d in range(16):
-                rgb_arr.append(int(encrypted_hex[t:t+2] , 16))
-                t+=2
-            for pixel in range(16):
-                        p_arr[x] = rgb_arr[pixel]
-                        x+=1
-                        if x>2:
-                            im1.putpixel((prev_col,j),(p_arr[0],p_arr[1],p_arr[2]))
-                            x%=3
-                            prev_col = (prev_col+1)
-                            i=prev_col
-                            if prev_col>212:
-                                j+=1
-                            prev_col%=213
-
-            
-print i
-g.close()
-im1.save("file_image.png")
-
-i=j=x=d=0
-rgb_arr = []
-hex_str=''
-im = Image.open("file_image.png")
-f = open("decrypted_image.png" , 'wb')
-for t in range(213*213):
-        #print i,j
-        if i>212:
-            i%=213
-            j+=1
-
-        r,g,b = im.getpixel((i,j))
-        x+=3
-
-        rgb_arr.append(r)
-        rgb_arr.append(g)
-        rgb_arr.append(b)
-
-        if x>16:
-            for z in range(16):
-                hex_val = hex(rgb_arr.pop(0))
-                if len(str(hex_val))==3:
-                    hex_val = hex_val[:2]+'0'+hex_val[2:]
-                hex_str += hex_val 
-            x%=16
-            hex_str = format_data(hex_str)
-            if d==0:
-                d=1
-                print "hex before decryption = ", hex_str
-            decrypted_hex = obj.decrypt(hex_str)
-            f.write(decrypted_hex)
-            hex_str = ''
-        i+=1
-f.close()         
-            
-'''
-        
-
-
-
-
